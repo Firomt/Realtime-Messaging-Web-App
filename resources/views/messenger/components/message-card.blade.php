d829b86b91cb2889ad60f913fee4b8ba15cf4b2d
Firomt <firomsamt@gmail.com>
2025-02-16 12:42:22 +0300

    feat: display message timestamps in human-readable format

--- resources/views/messenger/components/message-card.blade.php
+++ resources/views/messenger/components/message-card.blade.php
@@ -2,11 +2,10 @@
 <div class="wsus__single_chat_area">
     <div class="wsus__single_chat chat_right">
         <p class="messages">{{ $message->body }}</p>
-        <span class="time"> 5h ago</span>
+        <span class="time"> {{ timeAgo($message->created_at)}}</span>
         <a class="venobox" data-gall="gallery01" href="images/chat_img.png">
             <img src="images/chat_img.png" alt="gallery1" class="img-fluid w-100">
         </a>
-        <span class="time"> 5h ago</span>
-        <a class="action" href="#"><i class="fas fa-trash"></i></a>
+        
     </div>
 </div>
